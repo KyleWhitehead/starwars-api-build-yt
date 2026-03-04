d272204b3bda61fa2d646dbb7666cf05b0589932
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-03-04 14:03:43 -0700

    Commit 1.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import ClientLayout from "@/lib/client-layout";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body style={{ margin: 0, padding: 0, backgroundColor: "#222", color: "#fff" }}>
         <StyledComponentsRegistry>
           <ClientLayout>{children}</ClientLayout>
         </StyledComponentsRegistry>
